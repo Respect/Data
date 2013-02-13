2969fda8ca13bbfc76e97fe2b404951baf16bd32
Alexandre <alexandre@gaigalas.net>
2013-02-13 14:09:42 -0200

    Bug fixess

--- library/Respect/Data/Collections/Collection.php
+++ library/Respect/Data/Collections/Collection.php
@@ -93,14 +93,20 @@ class Collection implements ArrayAccess
     
     public function persist($object)
     {
-        if ($this->have('filters'))
-            return $this->mapper->persist($object, $this->next);
+        if ($this->have('filters')) {
+            $this->next->setMapper($this->mapper);
+            $this->next->persist($object);
+            return;
+        }
         
-        if (!$this->mapper)
+        if (!$this->mapper) {
             throw new \RuntimeException;
+        }
         
-        if ($this->next)
+        if ($this->next) {
+            $this->next->setMapper($this->mapper);
             $this->next->persist($object->{"{$this->next->getName()}_id"});
+        }
         
         foreach($this->children as $child)
             $child->persist($object->{"{$child->getName()}_id"});
