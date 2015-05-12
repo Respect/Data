0fa906f3fa18b554f2f8aff9709a49b605050f9f
nawarian <nawarian@gmail.com>
2015-05-11 22:54:29 -0300

    Finishing event dispatching under EventedMapper

--- library/Respect/Data/Event/EventedMapper.php
+++ library/Respect/Data/Event/EventedMapper.php
@@ -9,9 +9,12 @@ class EventedMapper
 
     protected $mapper;
 
+    protected $eventManager;
+
     public function __construct(AbstractMapper $mapper)
     {
         $this->mapper = $mapper;
+        $this->eventManager = new EventManager();
     }
 
     public function __get($name)
@@ -34,16 +37,69 @@ class EventedMapper
         return $this->mapper->__isset($alias);
     }
 
-    public function flush()
+    public function on($event, $callback)
     {
-        $this->notifyPreFlush();
-        $this->mapper->flush();
-        $this->notifyPostFlush();
+        return $this->eventManager->on($event, $callback);
     }
 
-    public function notifyPreFlush()
-    {}
+    public function flush()
+    {
+        $trackedQueue = $this->getTrackedQueue();
+        $trackedEntities = $this->getTrackedEntities();
 
-    public function notifyPostFlush()
-    {}
+        $this->processFlushQueue($trackedQueue, $trackedEntities, 'pre');
+        $this->mapper->flush();
+        $this->processFlushQueue($trackedQueue, $trackedEntities, 'post');
+    }
+
+    protected function processFlushQueue($queue, $trackedEntities, $eventSuffix)
+    {
+        $em = $this->eventManager;
+
+        foreach ($queue as $eventType => $objects) {
+            foreach ($objects as $entity) {
+                $collection = $trackedEntities[$entity];
+
+                $em->dispatch(
+                    "{$collection->getName()}:{$eventType}:{$eventSuffix}",
+                    array($entity, $collection)
+                );
+            }
+        }
+    }
+
+    protected function getTrackedQueue()
+    {
+        $m = $this->mapper;
+        $inserts = $this->getObjectPropertyFromReflection($m, 'new');
+        $updates = $this->getObjectPropertyFromReflection($m, 'changed');
+        $deletes = $this->getObjectPropertyFromReflection($m, 'removed');
+
+        return array(
+            'insert' => $inserts,
+            'update' => $updates,
+            'delete' => $deletes
+        );
+    }
+
+    protected function getTrackedEntities()
+    {
+        return $this->getObjectPropertyFromReflection(
+            $this->mapper,
+            'tracked'
+        );
+    }
+
+    private function getObjectPropertyFromReflection(
+        $object,
+        $property
+    ) {
+        $ref = new \ReflectionObject($object);
+        $refProp = $ref->getProperty($property);
+        if ($refProp->isPrivate() || $refProp->isProtected()) {
+            $refProp->setAccessible(true);
+        }
+
+        return $refProp->getValue($object);
+    }
 }
